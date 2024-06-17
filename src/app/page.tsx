8dee46f9b8af9e73fec5e736e44c2d2c427f8cd7
numberwang55 <cban0901@hotmail.com>
2024-06-17 11:30:27 +0100

    fetch data from db with server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+  const renderedSnippets = snippets.map(snippet => {
+    return (
+      <div key={snippet.id}>
+        <div>{snippet.title}</div>
+        <div>{snippet.code}</div>
+      </div>
+    )
+  })
+
   return (
-    <div>Home Page</div>
+    <div>{renderedSnippets}</div>
   );
 }
