7c0b0a40ab75eb637a2000c267ecadcdf0b040ee
numberwang55 <cban0901@hotmail.com>
2024-06-18 15:21:16 +0100

    loading component for snippets[id]

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -7,6 +7,8 @@ interface SnippetShowPageProps {
   };
 }
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((r) => setTimeout(r, 2000))
+
   const snippet = await db.snippet.findFirst({
     where: {
       id: +props.params.id,
