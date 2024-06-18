37fce897daf2ff7aad484856052f86a0fc820ecc
numberwang55 <cban0901@hotmail.com>
2024-06-18 15:41:53 +0100

    display snippets on homepage and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
-  const renderedSnippets = snippets.map(snippet => {
+  const renderedSnippets = snippets.map((snippet) => {
     return (
-      <div key={snippet.id}>
+      <Link
+        key={snippet.id}
+        className="flex justify-between items-center p-2 border rounded"
+        href={`/snippets/${snippet.id}`}
+      >
         <div>{snippet.title}</div>
-        <div>{snippet.code}</div>
-      </div>
-    )
-  })
+        <div>View</div>
+      </Link>
+    );
+  });
 
   return (
-    <div>{renderedSnippets}</div>
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href={"/snippets/new"} className="border p-2 rounded">New</Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
   );
 }
